16b515e0c4fa11ad6cc61f54c08572378081b5b2
TanviP28 <148252730+TanviP28@users.noreply.github.com>
2024-09-04 05:19:34 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,13 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDkeMHjKPXAOeHYDZPnWEzMJFmNN3bmSmE",
-    authDomain: "spendspot-7aff1.firebaseapp.com",
-    projectId: "spendspot-7aff1",
-    storageBucket: "spendspot-7aff1.appspot.com",
-    messagingSenderId: "590840142636",
-    appId: "1:590840142636:web:629381210c549658baea89",
-    measurementId: "G-2E9XDYV5YR"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
